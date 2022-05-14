1ed7ab4ba2841c43c1c1b80a11eeeae19417a1e6
Hungarian Chives <hungaria@si-c5.kzn.21-school.ru>
2022-05-14 17:09:44 +0300

    ibat4

--- src/memchr.c
+++ src/memchr.c
@@ -1,8 +1,7 @@
 #include "s21_string.h"
 
 void *s21_memchr(const void *str, int c, size_t n) {
-  unsigned char *s;
-  s = (unsigned char *)str;
+  unsigned char *s = (unsigned char *)str;
   while (n-- != 0) {
     if (*s == (unsigned char)c) {
       return (s);
